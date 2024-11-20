620a9056d2c57c7a3b2faebdb4f95654ca17d8c0
Amin Rafiee <93943199+aminrfe@users.noreply.github.com>
2024-11-20 22:49:21 +0330

    Set user as primary key in CustomerProfile model

--- back-end/snappfood/customer/models.py
+++ back-end/snappfood/customer/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from user.models import User
 
 class CustomerProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="customer_profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="customer_profile", primary_key=True)
     state = models.CharField(max_length=30)
     
     def __str__(self):

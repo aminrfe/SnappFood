52e36ddaafb561edf4c4abf8f2d9baafdf8d5986
MohammadNfa <NikfallahMohammad@gmail.com>
2024-11-20 22:12:17 +0330

    user role added

--- back-end/snappfood/user/models.py
+++ back-end/snappfood/user/models.py
@@ -1,13 +1,18 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
 
 class UserManager(BaseUserManager):
-    def create_user(self, phone_number, password=None, first_name=None, last_name=None, **extra_fields):
+    def create_user(self, phone_number, password=None, first_name=None, last_name=None, role="customer", **extra_fields):
         if not phone_number:
             raise ValueError("The Phone Number field must be set")
-        phone_number = self.normalize_email(phone_number)
-        user = self.model(phone_number=phone_number, first_name=first_name, last_name=last_name, **extra_fields)
+        user = self.model(
+            phone_number=phone_number,
+            first_name=first_name,
+            last_name=last_name,
+            role=role,
+            **extra_fields
+        )
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -21,12 +26,19 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self.create_user(phone_number, password, **extra_fields)
+        return self.create_user(phone_number, password, role="admin", **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = [
+        ("customer", "Customer"),
+        ("restaurant_manager", "Restaurant Manager"),
+        ("admin", "Admin"),
+    ]
+
     phone_number = models.CharField(max_length=15, unique=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True, null=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="customer")
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
@@ -36,4 +48,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['first_name']
 
     def __str__(self):
-        return self.phone_number
+        return f"{self.phone_number} ({self.role})"

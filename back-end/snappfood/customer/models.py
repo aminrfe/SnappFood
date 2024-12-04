6d0a2fe634fbe367080775889427f04e6a93469b
MohammadNikfallah <101511851+MohammadNikfallah@users.noreply.github.com>
2024-12-04 20:09:14 +0330

    customer fixed
    Signed-off-by: MohammadNikfallah <101511851+MohammadNikfallah@users.noreply.github.com>

--- back-end/snappfood/customer/models.py
+++ back-end/snappfood/customer/models.py
@@ -6,8 +6,6 @@ from django.conf import settings
 class CustomerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="customer_profile", primary_key=True)
     state = models.CharField(max_length=30)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
 
     
     def __str__(self):

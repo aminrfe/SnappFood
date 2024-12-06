c93ca8af4d3eda9b710bdfc331d3afcbd652a95f
Amin Rafiee <93943199+aminrfe@users.noreply.github.com>
2024-12-06 23:53:52 +0330

    Add menu items apis
    * add menu items apis
    
    * Fix menu items apis
    
    * Fix url paths

--- back-end/snappfood/restaurant/serializers.py
+++ back-end/snappfood/restaurant/serializers.py
@@ -1,15 +1,28 @@
 from rest_framework import serializers
-from .models import RestaurantProfile
+from .models import RestaurantProfile, Item
 
 class RestaurantProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = RestaurantProfile
         fields = [
             'name', 'business_type', 'delivery_price', 'address',
-            'description', 'open_hour', 'close_hour', 'photo', 'latitude', 'longitude'
+            'description', 'open_hour', 'close_hour', 'latitude', 'longitude', 'photo'
         ]
 
     def validate_photo(self, value):
         if value and not value.name.lower().endswith(('jpg', 'jpeg', 'png')):
             raise serializers.ValidationError("Photo must be in JPEG or PNG format.")
         return value
+    
+
+class ItemCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['price', 'discount', 'name', 'description', 'state', 'photo']
+
+
+class ItemListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['id', 'price', 'discount', 'name', 'score', 'description', 'state', 'photo']
+
--- back-end/snappfood/restaurant/urls.py
+++ back-end/snappfood/restaurant/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RestaurantProfileView
+from .views import RestaurantProfileView, ItemListCreateView, ItemRetrieveUpdateView
 
 urlpatterns = [
-    path('<int:pk>/profile', RestaurantProfileView.as_view(), name='restaurant-profile'),
+    path('<int:id>/profile', RestaurantProfileView.as_view(), name='restaurant-profile'),
+    path('<int:restaurant_id>/items', ItemListCreateView.as_view(), name='item-list-create'),
+    path('<int:restaurant_id>/items/<int:id>', ItemRetrieveUpdateView.as_view(), name='item-retrieve-update'),
 ]

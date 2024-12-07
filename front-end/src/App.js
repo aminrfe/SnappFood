dcdbc60f839add532d3aeaae63791041f357371a
neginq <neg.qorbani@gmail.com>
2024-12-07 17:20:04 +0330

    favorite list page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import RTLProvider from "./ThemeProvider";
 import HomePage from "./components/User/HomePage";
@@ -8,69 +8,61 @@ import EditProfile from "./components/User/EditProfile";
 import Header from "./components/User/Header";
 import UserSignUp from "./components/User/SignUp";
 import RestaurantSignUp from "./components/Restaurant/SignUp";
-import UserProvider from "./components/User/UserContext"; 
+import UserProvider from "./components/User/UserContext";
 import FoodItemPage from "./components/User/MenuItem";
+import FavoritesPage from "./components/User/FavoritesPage.js";
+
 import "./App.css";
 
-
-function App() {function isAuthenticated() {
+function App() {
+  function isAuthenticated() {
     const accessToken = localStorage.getItem("access");
     const refreshToken = localStorage.getItem("refresh");
-    return !!(accessToken && refreshToken); 
+    return Boolean(accessToken && refreshToken); // Return true if tokens exist
   }
 
-
   return (
-		<div className="app-container">
-			<RTLProvider>
-				<UserProvider>
-					<Router>
-						<Routes>
-							<Route
-								path="/"
-								element={
-									<>
-										<Header isAuthenticated={isAuthenticated} />
-										<HomePage isAuthenticated={isAuthenticated} />
-									</>
-								}
-							/>
-
-							{/* <Route
-              path="/profile"
-              element={isAuthenticated() ? <UserProfilePage /> : <Navigate to="/login" />}
-            />
-            <Route
-              path="/edit-profile"
-              element={isAuthenticated() ? <EditProfilePage /> : <Navigate to="/login" />}
-            /> */}
-
-							<Route path="/login" element={<LoginPage />} />
-							<Route path="/userSignUp" element={<UserSignUp />} />
-							<Route path="/menu-item" element={<FoodItemPage />} />
-							<Route path="/restaurantSignUp" element={<RestaurantSignUp />} />
-							<Route
-								path="/profile"
-								element={
-									isAuthenticated() ? (
-										<UserProfilePage />
-									) : (
-										<Navigate to="/login" />
-									)
-								}
-							/>
-							<Route
-								path="/edit-profile"
-								element={
-									isAuthenticated() ? <EditProfile /> : <Navigate to="/login" />
-								}
-							/>
-						</Routes>
-					</Router>
-				</UserProvider>
-			</RTLProvider>
-		</div>
-	);
+    <div className="app-container">
+      <RTLProvider>
+        <UserProvider>
+          <Router>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Header isAuthenticated={isAuthenticated} />
+                    <HomePage isAuthenticated={isAuthenticated} />
+                  </>
+                }
+              />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/userSignUp" element={<UserSignUp />} />
+              <Route path="/menu-item" element={<FoodItemPage />} />
+              <Route path="/restaurantSignUp" element={<RestaurantSignUp />} />
+              <Route
+                path="/profile"
+                element={
+                  isAuthenticated() ? (
+                    <UserProfilePage />
+                  ) : (
+                    <Navigate to="/login" />
+                  )
+                }
+              />
+              <Route
+                path="/edit-profile"
+                element={
+                  isAuthenticated() ? <EditProfile /> : <Navigate to="/login" />
+                }
+              />
+              <Route path="/favorites" element={<FavoritesPage />} />
+            </Routes>
+          </Router>
+        </UserProvider>
+      </RTLProvider>
+    </div>
+  );
 }
 
 export default App;

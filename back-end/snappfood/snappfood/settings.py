07f7a5cb747fa50e7fa4b20a0e7f5aae3237c955
Amin Rafiee <93943199+aminrfe@users.noreply.github.com>
2024-12-26 22:07:52 +0330

    Add database configuration via .env

--- back-end/snappfood/snappfood/settings.py
+++ back-end/snappfood/snappfood/settings.py
@@ -89,17 +89,13 @@ WSGI_APPLICATION = 'snappfood.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'snappfood',        
-    #     'USER': 'postgres',       
-    #     'PASSWORD': '1234',     
-    #     'HOST': 'localhost',            
-    #     'PORT': '5432',               
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': env('DATABASE_ENGINE', default='django.db.backends.sqlite3'),
+        'NAME': env('DATABASE_NAME', default=os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': env('DATABASE_USER', default=''),
+        'PASSWORD': env('DATABASE_PASSWORD', default=''),
+        'HOST': env('DATABASE_HOST', default=''),
+        'PORT': env('DATABASE_PORT', default=''),
     }
 }
 

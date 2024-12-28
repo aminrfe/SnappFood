6685711293621d47810827b6713f86ca3fcea1ea
Amin Rafiee <93943199+aminrfe@users.noreply.github.com>
2024-12-28 18:33:29 +0330

    Fix some issues

--- back-end/snappfood/user/management/commands/createsuperuser_if_not_exists.py
+++ back-end/snappfood/user/management/commands/createsuperuser_if_not_exists.py
@@ -7,11 +7,23 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")  
-        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin123") 
 
-        if not User.objects.filter(is_superuser=True).exists():
-            User.objects.create_superuser(username=username, password=password)
-            self.stdout.write(self.style.SUCCESS("Superuser created successfully."))
+        # Fetch environment variables for superuser creation
+        phone_number = os.getenv("DJANGO_SUPERUSER_PHONE_NUMBER", "989114098712")
+        first_name = os.getenv("DJANGO_SUPERUSER_FIRST_NAME", "Amin")
+        last_name = os.getenv("DJANGO_SUPERUSER_LAST_NAME", "Rafiee")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin123")
+        role = os.getenv("DJANGO_SUPERUSER_ROLE", "admin")  
+
+        if not User.objects.filter(phone_number=phone_number, is_superuser=True).exists():
+            User.objects.create_superuser(
+                phone_number=phone_number,
+                password=password,
+                first_name=first_name,
+                last_name=last_name,
+                role=role,
+            )
+            self.stdout.write(self.style.SUCCESS(f"Superuser created successfully with phone number {phone_number}."))
         else:
             self.stdout.write(self.style.WARNING("Superuser already exists."))
+

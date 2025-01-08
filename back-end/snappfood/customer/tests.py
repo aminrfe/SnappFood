f9e792c766a52611d084ec7936ec6caeb4e8a7b2
MohammadNfa <NikfallahMohammad@gmail.com>
2025-01-09 02:47:01 +0330

    TestFavoriteView done

--- back-end/snappfood/customer/tests.py
+++ back-end/snappfood/customer/tests.py
@@ -3,7 +3,8 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth import get_user_model
 
-from .models import CustomerProfile
+from .models import CustomerProfile, Favorite
+from restaurant.models import RestaurantProfile
 from .serializers import CustomerProfileSerializer
 
 User = get_user_model()
@@ -56,3 +57,70 @@ class TestCustomerProfileView(APITestCase):
         self.assertEqual(self.customer_profile.address, "456 New Address")
 
 
+class TestFavoriteView(APITestCase):
+    def setUp(self):
+        self.customer_user = User.objects.create_user(
+            phone_number="4445556666",
+            password="test_pass",
+            first_name="Alice",
+            role="customer",
+        )
+        self.customer_profile = CustomerProfile.objects.create(
+            user=self.customer_user,
+            address="Test Address",
+        )
+
+        self.manager_user = User.objects.create_user(
+            phone_number="7778889999",
+            password="manager_pass",
+            first_name="Bob",
+            role="restaurant_manager",
+        )
+
+        self.manager_user2 = User.objects.create_user(
+            phone_number="6778889999",
+            password="manager_pass",
+            first_name="Bob2",
+            role="restaurant_manager",
+        )
+
+        # Create restaurants
+        self.restaurant1 = RestaurantProfile.objects.create(manager=self.manager_user, name="Restaurant A")
+        self.restaurant2 = RestaurantProfile.objects.create(manager=self.manager_user2, name="Restaurant B")
+
+        # Authenticate as the customer
+        self.client.force_authenticate(user=self.customer_user)
+
+        # Assume your URL for this view is named 'favorite-list'
+        self.url = reverse("customer-favorite-restaurants")
+
+
+    def test_get_favorites(self):
+        Favorite.objects.create(user=self.customer_user, restaurant=self.restaurant1)
+        Favorite.objects.create(user=self.customer_user, restaurant=self.restaurant2)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_post_add_favorite_success(self):
+        """
+        Test adding a restaurant to favorites successfully.
+        """
+        data = {"restaurant_id": self.restaurant1.id}
+        response = self.client.post(self.url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn("restaurant", response.data)
+        # Verify that the favorite was created in the database
+        self.assertTrue(Favorite.objects.filter(user=self.customer_user, restaurant=self.restaurant1).exists())
+
+    def test_delete_favorite_success(self):
+        """
+        Test removing a restaurant from favorites successfully.
+        """
+        # First, add a favorite
+        favorite = Favorite.objects.create(user=self.customer_user, restaurant=self.restaurant1)
+        # Now delete it using query param ?restaurant_id=<id>
+        delete_url = f"{self.url}?restaurant_id={self.restaurant1.id}"
+        response = self.client.delete(delete_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Favorite.objects.filter(id=favorite.id).exists())

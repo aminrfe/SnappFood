160d31273df41b84282de41033b5f1925d1c8a57
Melika Eghbal <meleghbal@gmail.com>
2025-01-29 02:32:02 +0330

    Customer orders integrated.

--- front-end/src/components/User/MyOrders.js
+++ front-end/src/components/User/MyOrders.js
@@ -1,193 +1,277 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
-  Box,
-  Typography,
-  Card,
-  CardMedia,
-  CardContent,
-  Button,
-  Grid,
-  Toolbar,
-  AppBar,
+	Box,
+	Typography,
+	Toolbar,
+	AppBar,
+	Button,
+	Card,
+	CardContent,
+	CardActions,
+	Collapse,
+	List,
+	ListItem,
+	ListItemText,
 } from "@mui/material";
+import moment from "moment-jalaali";
+import Grid from "@mui/material/Grid2";
 import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utills/axiosInstance";
 import FoodiLogo from "../../assets/imgs/foodiIcon.png";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const deliveryMethodMap = {
+	pickup: "دریافت حضوری",
+	delivery: "ارسال با پیک",
+};
+
+const paymentMethodMap = {
+	in_person: "نقدی",
+	online: "آنلاین",
+};
 
 const MyOrders = () => {
-   const navigate = useNavigate();
-  const orders = [
-    {
-      id: 1,
-      title: "سفارش شماره یک",
-      date: "۱۸:۰۷ | یک شنبه",
-      price: 365500,
-      status: "تحویل شده",
-      details: "اطلاعات مربوط به سفارش شامل توضیحات و...",
-      image: require("../../assets/imgs/food1.png"),
-    },
-    {
-      id: 2,
-      title: "سفارش شماره دو",
-      date: "۱۸:۰۷ | یک شنبه",
-      price: 365500,
-      status: "تحویل شده",
-      details: "اطلاعات مربوط به سفارش شامل توضیحات و...",
-      image: require("../../assets/imgs/food1.png"),
-    },
-    {
-      id: 3,
-      title: "سفارش شماره سه",
-      date: "۱۸:۰۷ | یک شنبه",
-      price: 365500,
-      status: "تحویل شده",
-      details: "اطلاعات مربوط به سفارش شامل توضیحات و...",
-      image: require("../../assets/imgs/food1.png"),
-    },
-    {
-      id: 4,
-      title: "سفارش شماره چهار",
-      date: "۱۸:۰۷ | یک شنبه",
-      price: 365500,
-      status: "تحویل شده",
-      details: "اطلاعات مربوط به سفارش شامل توضیحات و...",
-      image: require("../../assets/imgs/food1.png"),
-    },
-    {
-      id: 5,
-      title: "سفارش شماره چهار",
-      date: "۱۸:۰۷ | یک شنبه",
-      price: 365500,
-      status: "تحویل شده",
-      details: "اطلاعات مربوط به سفارش شامل توضیحات و...",
-      image: require("../../assets/imgs/food1.png"),
-    },
-    {
-      id: 6,
-      title: "سفارش شماره چهار",
-      date: "۱۸:۰۷ | یک شنبه",
-      price: 365500,
-      status: "تحویل شده",
-      details: "اطلاعات مربوط به سفارش شامل توضیحات و...",
-      image: require("../../assets/imgs/food1.png"),
-    },
-  ];
+	const [orders, setOrders] = useState([]);
+	const [error, setError] = useState(null);
+	const [openIndex, setOpenIndex] = useState(null); // To track the open collapse
+	const navigate = useNavigate();
 
-  return (
-    <Box
-      sx={{
-        backgroundColor: "#F5F5F5",
-        minHeight: "100vh",
-      }}
-    >
-      <AppBar
-        position="static"
-        sx={{
-          backgroundColor: "#F4DCC9",
-          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-        }}
-      >
-        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-          <a
-            href="/"
-            onClick={(e) => {
-              e.preventDefault();
-              navigate("/");
-            }}
-          >
-            <img
-              src={FoodiLogo}
-              alt="Foodi Logo"
-              style={{ width: "100px", cursor: "pointer" }}
-            />
-          </a>
-          <Typography
-            variant="h6"
-            sx={{
-              color: "#D68240",
-              fontWeight: "bold",
-              flex: 1,
-              textAlign: "center",
-              userSelect: "none",
-              pointerEvents: "none",
-              paddingRight: "50px",
-            }}
-          >
-           سفارش های من
-          </Typography>
-          <Box />
-        </Toolbar>
-      </AppBar>
+	useEffect(() => {
+		axiosInstance
+			.get("/customer/orders")
+			.then((response) => {
+				console.log("Fetched Orders:", response.data);
+				setOrders(response.data);
+			})
+			.catch((err) => {
+				setError("خطا در بارگذاری سفارش‌ها");
+			});
+	}, []);
 
-      <Grid container spacing={2} justifyContent="center" padding="20px">
-        {orders.map((order) => (
-          <Grid item xs={12} sm={6} md={4} key={order.id} >
-            <Card
-              sx={{
-                display: "flex",
-                flexDirection: { xs: "column", sm: "row" },
-                alignItems: "center",
-                padding: 2,
-                backgroundColor: "#FFF5ED",
-                borderRadius: 2,
-                boxShadow: 1,
-              }}
-            >
-              <CardMedia
-                component="img"
-                image={order.image}
-                alt={order.title}
-                sx={{
-                  width: { xs: 80, sm: 100 },
-                  height: { xs: 80, sm: 100 },
-                  borderRadius: 2,
-                  objectFit: "cover",
-                  marginLeft: { sm: 2 },
-                  marginBottom: { xs: 2, sm: 0 },
-                }}
-              />
-              <CardContent sx={{ flex: 1 }}>
-                <Typography
-                  variant="h6"
-                  fontWeight="bold"
-                  sx={{ marginBottom: 1, color: "#333" }}
-                >
-                  {order.title}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                  sx={{ marginBottom: 1 }}
-                >
-                  {order.date}
-                </Typography>
-                <Typography
-                  variant="body1"
-                  fontWeight="bold"
-                  sx={{ color: "#D68240", marginBottom: 1 }}
-                >
-                  {order.price.toLocaleString()} تومان
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                  sx={{ marginBottom: 2 }}
-                >
-                  {order.details}
-                </Typography>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  sx={{ backgroundColor: "#FF7043" }}
-                >
-                  سفارش مجدد
-                </Button>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
-  );
+	const handleCollapseToggle = (index) => {
+		setOpenIndex(openIndex === index ? null : index); // Toggle collapse
+	};
+
+	return (
+		<Box sx={{ padding: 0 }}>
+			<AppBar
+				position="static"
+				sx={{
+					backgroundColor: "#F4DCC9",
+					boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+				}}
+			>
+				<Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+					<a
+						href="/"
+						onClick={(e) => {
+							e.preventDefault();
+							navigate("/");
+						}}
+					>
+						<img
+							src={FoodiLogo}
+							alt="Foodi Logo"
+							style={{ width: "100px", cursor: "pointer" }}
+						/>
+					</a>
+					<Typography
+						variant="h6"
+						sx={{
+							color: "#D68240",
+							fontWeight: "bold",
+							flex: 1,
+							textAlign: "center",
+							userSelect: "none",
+							pointerEvents: "none",
+							paddingRight: "50px",
+						}}
+					>
+						سفارش‌های من
+					</Typography>
+					<Box />
+				</Toolbar>
+			</AppBar>
+
+			<Grid container spacing={2} justifyContent="center" padding="20px">
+				{orders.length === 0 ? (
+					<Typography
+						variant="h6"
+						color="text.secondary"
+						style={{ marginTop: "50px", marginRight: "25px" }}
+					>
+						لیست سفارش‌های شما خالی است.
+					</Typography>
+				) : (
+					orders.map((order, index) => (
+						<Card
+							key={order.id}
+							sx={{
+								flexDirection: { xs: "column", sm: "row" },
+								marginBottom: "1rem",
+								width: "70%",
+								backgroundColor: "#FFF5ED",
+								boxShadow: 2,
+								borderRadius: 2,
+								alignItems: "center",
+								display: "flex",
+								justifyContent: "space-between",
+							}}
+						>
+							<CardContent>
+								<Box
+									sx={{
+										display: "flex",
+										flexDirection: "column",
+										alignItems: "flex-start",
+									}}
+								>
+									<Typography
+										variant="h6"
+										fontWeight="bold"
+										sx={{ marginBottom: 1, color: "#333" }}
+									>
+										سفارش شماره {order.order_id}
+									</Typography>
+									<Typography
+										variant="body2"
+										color="text.secondary"
+										sx={{ marginBottom: 3 }}
+									>
+										{order.description}
+									</Typography>
+									<Typography
+										variant="body2"
+										color="text.secondary"
+										sx={{ marginBottom: 1 }}
+									>
+										تاریخ سفارش:{" "}
+										{moment(order.order_date).format("jYYYY/jMM/jDD")} در ساعت{" "}
+										{moment(order.order_date).format("HH:mm")}
+									</Typography>
+									<Typography
+										variant="body2"
+										color="text.secondary"
+										sx={{
+											fontSize: { xs: "0.75rem", sm: "0.875rem" },
+											marginBottom: 1,
+										}}
+									>
+										آدرس: {order.address}
+									</Typography>
+									<Typography
+										variant="body2"
+										color="text.secondary"
+										sx={{
+											fontSize: { xs: "0.75rem", sm: "0.875rem" },
+											marginBottom: 1,
+										}}
+									>
+										روش ارسال: {deliveryMethodMap[order.delivery_method]}
+									</Typography>
+									<Typography
+										variant="body2"
+										color="text.secondary"
+										sx={{
+											fontSize: { xs: "0.75rem", sm: "0.875rem" },
+											marginBottom: 2,
+										}}
+									>
+										روش پرداخت: {paymentMethodMap[order.payment_method]}
+									</Typography>
+									<Typography
+										variant="body1"
+										fontWeight="bold"
+										sx={{ color: "#D68240", marginBottom: 1 }}
+									>
+										قیمت کل: {Math.floor(order.total_price).toLocaleString()}{" "}
+										تومان
+									</Typography>
+								</Box>
+							</CardContent>
+
+							<CardActions>
+								<Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+									{order.status === "completed" ? (
+										<Button variant="contained" disabled>
+											تحویل شده
+										</Button>
+									) : (
+										<Button
+											variant="contained"
+											onClick={(event) => {
+												event.stopPropagation();
+												navigate(
+													`/customer/orders/${order.order_id}/track-order`,
+												);
+											}}
+										>
+											پیگیری سفارش
+										</Button>
+									)}
+									<Button
+										variant="contained"
+										onClick={(event) => {
+											event.stopPropagation();
+											navigate(`/customer/restaurants/${order.restaurant}`);
+										}}
+									>
+										سفارش مجدد
+									</Button>
+								</Box>
+
+								<Button
+									onClick={() => handleCollapseToggle(index)}
+									sx={{
+										backgroundColor: "#D68240",
+										color: "white",
+										padding: "6px 12px",
+										borderRadius: "4px",
+										"&:hover": { backgroundColor: "#D68240" },
+									}}
+								>
+									<ExpandMoreIcon />
+								</Button>
+							</CardActions>
+
+							<Collapse in={openIndex === index} timeout="auto" unmountOnExit>
+								<CardContent>
+									<Box>
+										<Typography
+											variant="body1"
+											sx={{
+												fontWeight: "bold",
+												marginBottom: "0.5rem",
+												fontSize: { xs: "0.875rem", sm: "1rem" },
+											}}
+										>
+											آیتم‌های سفارش:
+										</Typography>
+										<List>
+											{order.order_items.map((item, idx) => (
+												<ListItem
+													key={idx}
+													sx={{
+														paddingLeft: 0,
+														fontSize: { xs: "0.75rem", sm: "0.875rem" },
+													}}
+												>
+													<ListItemText
+														primary={`${item.name} - تعداد: ${item.count}`}
+														sx={{ fontSize: { xs: "0.75rem", sm: "0.875rem" } }}
+													/>
+												</ListItem>
+											))}
+										</List>
+									</Box>
+								</CardContent>
+							</Collapse>
+						</Card>
+					))
+				)}
+			</Grid>
+		</Box>
+	);
 };
 
 export default MyOrders;
--- front-end/src/components/User/TrackOrder.js
+++ front-end/src/components/User/TrackOrder.js
@@ -1,167 +1,209 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate  } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import {
-  Box,
-  Typography,
-  Stepper,
-  Step,
-  StepLabel,
-  Paper,
+	Box,
+	Typography,
+	Stepper,
+	Step,
+	StepLabel,
+	Paper,
+	AppBar,
+	Toolbar,
 } from "@mui/material";
+import FoodiLogo from "../../assets/imgs/foodiIcon.png";
 import axiosInstance from "../../utills/axiosInstance";
 import MotorbikeImage from "../../assets/imgs/motorbike.png";
 
 const TrackOrderPage = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const [order, setOrder] = useState(null);
-  const [activeStep, setActiveStep] = useState(0);
+	const { id } = useParams();
+	const navigate = useNavigate();
+	const [order, setOrder] = useState(null);
+	const [activeStep, setActiveStep] = useState(0);
 
-  const steps = [
-    "در انتظار تایید",
-    "در حال آماده سازی",
-    "ارسال شده",
-    "تحویل داده شده",
-  ];
+	const steps = [
+		"در انتظار تایید",
+		"در حال آماده سازی",
+		"ارسال شده",
+		"تحویل داده شده",
+	];
 
-  useEffect(() => {
-    const fetchOrder = async () => {
-      try {
-        const response = await axiosInstance.get("/customer/orders");
-        // console.log(response.data);
-        const fetchedOrder = response.data.find(
-          (order) => order.order_id.toString() === id
-        );
+	useEffect(() => {
+		const fetchOrder = async () => {
+			try {
+				const response = await axiosInstance.get("/customer/orders");
+				// console.log(response.data);
+				const fetchedOrder = response.data.find(
+					(order) => order.order_id.toString() === id,
+				);
 
-        setOrder(fetchedOrder);
-        switch (fetchedOrder.state) {
-          case "pending":
-            setActiveStep(0);
-            break;
-          case "preparing":
-            setActiveStep(1);
-            break;
-          case "delivering":
-            setActiveStep(2);
-            break;
-          case "completed":
-            setActiveStep(3);
-            navigate(`/customer/orders/${id}/review`);
-            break;
-          default:
-            setActiveStep(0);
-        }
-      } catch (error) {
-        console.error("خطا در دریافت اطلاعات سفارش:", error);
-      }
-    };
+				setOrder(fetchedOrder);
+				switch (fetchedOrder.state) {
+					case "pending":
+						setActiveStep(0);
+						break;
+					case "preparing":
+						setActiveStep(1);
+						break;
+					case "delivering":
+						setActiveStep(2);
+						break;
+					case "completed":
+						setActiveStep(3);
+						navigate(`/customer/orders/${id}/review`);
+						break;
+					default:
+						setActiveStep(0);
+				}
+			} catch (error) {
+				console.error("خطا در دریافت اطلاعات سفارش:", error);
+			}
+		};
 
-    fetchOrder();
-    const interval = setInterval(fetchOrder, 5000); 
-    return () => clearInterval(interval); 
-  }, [id, navigate]);
+		fetchOrder();
+		const interval = setInterval(fetchOrder, 5000);
+		return () => clearInterval(interval);
+	}, [id, navigate]);
 
-  if (!order) {
-    return (
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "100vh",
-          backgroundColor: "#F5F5F5",
-        }}
-      >
-        <Typography>در حال بارگذاری...</Typography>
-      </Box>
-    );
-  }
+	if (!order) {
+		return (
+			<Box
+				sx={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					minHeight: "100vh",
+					backgroundColor: "#F5F5F5",
+				}}
+			>
+				<Typography>در حال بارگذاری...</Typography>
+			</Box>
+		);
+	}
 
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh",
-        backgroundColor: "#F5F5F5",
-        padding: "20px",
-      }}
-    >
-      <Paper
-        elevation={3}
-        sx={{
-          width: "70%",
-          textAlign: "center",
-          padding: "30px",
-          borderRadius: "10px",
-          backgroundColor: "#FFEDE0",
-        }}
-      >
-        {/* عنوان */}
-        <Typography
-          variant="h5"
-          fontWeight="bold"
-          gutterBottom
-          sx={{ fontSize: "1.8rem" }}
-        >
-          وضعیت سفارش شما
-        </Typography>
+	return (
+		<Box sx={{ padding: 0 }}>
+			<AppBar
+				position="static"
+				sx={{
+					backgroundColor: "#F4DCC9",
+					boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+				}}
+			>
+				<Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+					<a
+						href="/"
+						onClick={(e) => {
+							e.preventDefault();
+							navigate("/");
+						}}
+					>
+						<img
+							src={FoodiLogo}
+							alt="Foodi Logo"
+							style={{ width: "100px", cursor: "pointer" }}
+						/>
+					</a>
+					<Typography
+						variant="h6"
+						sx={{
+							color: "#D68240",
+							fontWeight: "bold",
+							flex: 1,
+							textAlign: "center",
+							userSelect: "none",
+							pointerEvents: "none",
+							paddingRight: "50px",
+						}}
+					>
+						سفارش‌های من
+					</Typography>
+					<Box />
+				</Toolbar>
+			</AppBar>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					justifyContent: "center",
+					minHeight: "100vh",
+					backgroundColor: "#F5F5F5",
+					padding: "20px",
+				}}
+			>
+				<Paper
+					elevation={3}
+					sx={{
+						width: "70%",
+						textAlign: "center",
+						padding: "30px",
+						borderRadius: "10px",
+						backgroundColor: "#FFEDE0",
+					}}
+				>
+					{/* عنوان */}
+					<Typography
+						variant="h5"
+						fontWeight="bold"
+						gutterBottom
+						sx={{ fontSize: "1.8rem" }}
+					>
+						وضعیت سفارش شما
+					</Typography>
 
-        {/* اطلاعات */}
-        <Typography
-          variant="body1"
-          gutterBottom
-          sx={{
-            marginBottom: "40px",
-            fontSize: "1.2rem",
-          }}
-        >
-          سفارش شما به ثبت رسید.{" "}
-          {order.description ? `توضیحات: ${order.description}` : ""}
-        </Typography>
+					{/* اطلاعات */}
+					<Typography
+						variant="body1"
+						gutterBottom
+						sx={{
+							marginBottom: "40px",
+							fontSize: "1.2rem",
+						}}
+					>
+						سفارش شما به ثبت رسید.{" "}
+					</Typography>
 
-        {/* تصویر */}
-        <Box sx={{ marginY: 3 }}>
-          <img
-            src={MotorbikeImage}
-            alt="Motorbike"
-            style={{
-              width: "250px",
-              height: "auto",
-            }}
-          />
-        </Box>
+					{/* تصویر */}
+					<Box sx={{ marginY: 3 }}>
+						<img
+							src={MotorbikeImage}
+							alt="Motorbike"
+							style={{
+								width: "250px",
+								height: "auto",
+							}}
+						/>
+					</Box>
 
-        {/* Stepper */}
-        <Stepper
-          activeStep={activeStep}
-          alternativeLabel
-          sx={{
-            "& .MuiStepConnector-line": {
-              borderColor: "#FFA726",
-            },
-            marginBottom: "40px",
-          }}
-        >
-          {steps.map((label, index) => (
-            <Step key={index}>
-              <StepLabel
-                StepIconProps={{
-                  sx: {
-                    color: index <= activeStep ? "#FFA726" : "gray",
-                  },
-                }}
-              >
-                {label}
-              </StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-      </Paper>
-    </Box>
-  );
+					{/* Stepper */}
+					<Stepper
+						activeStep={activeStep}
+						alternativeLabel
+						sx={{
+							"& .MuiStepConnector-line": {
+								borderColor: "#FFA726",
+							},
+							marginBottom: "40px",
+						}}
+					>
+						{steps.map((label, index) => (
+							<Step key={index}>
+								<StepLabel
+									StepIconProps={{
+										sx: {
+											color: index <= activeStep ? "#FFA726" : "gray",
+										},
+									}}
+								>
+									{label}
+								</StepLabel>
+							</Step>
+						))}
+					</Stepper>
+				</Paper>
+			</Box>
+		</Box>
+	);
 };
 
 export default TrackOrderPage;

73b4e5a62f9df1c5a4561bcb3608f016cb6ec2e4
SetarehPishiyar <101051739+SetarehPishiyar@users.noreply.github.com>
2025-01-29 11:56:00 +0330

    Restaurant admin approve (#110)
    * admin approvement added
    
    * admin approve

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -81,10 +81,18 @@ function Login() {
       localStorage.setItem("res_id", response.data.restaurant_id);
       localStorage.setItem("phone", phoneNumber);
 
-      if (response.data.restaurant_id)
-        navigate(`/restaurant/${response.data.restaurant_id}/profile`);
-      else navigate("/");
-      window.location.reload();
+      if (response.data.restaurant_id) {
+        if (response.data.state === "approved") {
+          navigate(`/restaurant/${response.data.restaurant_id}/profile`);
+        } else if (response.data.state === "pending") {
+          alert("فروشگاه شما در انتظار تایید ادمین است");
+        } else if (response.data.state === "rejected") {
+          alert("فروشگاه شما توسط ادمین رد شده است");
+        }
+      } else {
+        navigate("/");
+        window.location.reload();
+      }
     } catch (error) {
       if (error.response?.status === 401) {
         setError("اطلاعات ورود صحیح نیست.");
